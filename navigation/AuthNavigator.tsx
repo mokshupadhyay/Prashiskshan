1233b0f893db591d5c35f33591b8d4af51b8c358
Moksh Upadhyay <mokshupadhyay4@gmail.com>
2025-10-10 23:58:39 +0530

    Update navigation/AuthNavigator.tsx

--- navigation/AuthNavigator.tsx
+++ navigation/AuthNavigator.tsx
@@ -4,6 +4,10 @@ import { Landing } from '../src/screens/Auth/Landing.screen';
 import { Login } from '../src/screens/Auth/Login.screen';
 import { OTP } from '../src/screens/Auth/OTP.screen';
 import { UserDetails } from '../src/screens/Auth/UserDetails.screen';
+import { RoleSelection } from '../src/screens/Auth/RoleSelection.screen';
+import { StudentProfileSetup } from '../src/screens/Auth/StudentProfileSetup.screen';
+import { MentorProfileSetup } from '../src/screens/Auth/MentorProfileSetup.screen';
+import { RecruiterProfileSetup } from '../src/screens/Auth/RecruiterProfileSetup.screen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +18,10 @@ export const AuthNavigator = () => {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="OTP" component={OTP} />
       <Stack.Screen name="UserDetails" component={UserDetails} />
+      <Stack.Screen name="RoleSelection" component={RoleSelection} />
+      <Stack.Screen name="StudentProfileSetup" component={StudentProfileSetup} />
+      <Stack.Screen name="MentorProfileSetup" component={MentorProfileSetup} />
+      <Stack.Screen name="RecruiterProfileSetup" component={RecruiterProfileSetup} />
     </Stack.Navigator>
   );
 };
